78f84063a4a1bc61c66f97c121cadee6e15dd4d8
rubensdellead <94568920+rubensdellead@users.noreply.github.com>
2021-11-20 16:29:59 -0300

    Update AP1_vetor.cpp

--- AP1_vetor.cpp
+++ AP1_vetor.cpp
@@ -10,7 +10,7 @@ bool verif_crescente(int v[],int n){
     int cont = 1; //(c2) (1x)
     if(n > 1){ //(c3) (1x)
     for(int i = 1; i < n; i++){ //PIOR caso:(c4(n vezes));
-    				            // MELHOR caso: ele rodará uma ÚNICA vez; 
+    				  // MELHOR caso: ele rodará uma ÚNICA vez; 
         if(v[i-1] < v[i]){ /*PIOR caso: (c5) Rodará n vezes no , e no MELHOR caso rodará uma ÚNICA vez, pois, 
                             logo no primeiro teste, teremos que: o elemento que tiver na posição i será
                             maior que o elemento que se encontra na posição i-1. */
